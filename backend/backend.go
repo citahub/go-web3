890f567d1046110068a43ba5ca45c4e24122eb73
yejiayu <yejiayu.fe@gmail.com>
2018-07-16 09:43:11 +0800

    Update LICENSE

--- backend/backend.go
+++ backend/backend.go
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
-// Modify from https://github.com/ethereum/go-ethereum/blob/master/accounts/abi/bind/backend.go
 package backend
 
 import (
